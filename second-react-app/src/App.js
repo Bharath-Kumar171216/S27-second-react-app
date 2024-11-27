b7af19455a33bbae08ac87c47d4a6b39c288cd4c
Bharath Kumar <bharathveeravalli786@gmail.com>
2024-11-27 23:31:20 +0530

    Add files via upload

---
+++ second-react-app/src/App.js
@@ -0,0 +1,23 @@
+  import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This heading is created in src/App.js and displayed in public/index.html</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.4EmF65rDW41av0YAWfdFBgHaE2&pid=Api&P=0&h=180"></img>
+      <h1>This heading is created in src/App.js and displayed in public/index.html</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.4EmF65rDW41av0YAWfdFBgHaE2&pid=Api&P=0&h=180"></img>
+      <h1>This heading is created in src/App.js and displayed in public/index.html</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.4EmF65rDW41av0YAWfdFBgHaE2&pid=Api&P=0&h=180"></img>
+      <h1>This heading is created in src/App.js and displayed in public/index.html</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.4EmF65rDW41av0YAWfdFBgHaE2&pid=Api&P=0&h=180"></img>
+      <h1>This heading is created in src/App.js and displayed in public/index.html</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.4EmF65rDW41av0YAWfdFBgHaE2&pid=Api&P=0&h=180"></img>
+      <h1>This heading is created in src/App.js and displayed in public/index.html</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.4EmF65rDW41av0YAWfdFBgHaE2&pid=Api&P=0&h=180"></img>
+    </div>
+  );
+}
+
+export default App;
